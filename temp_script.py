11ce8ccf8df94b0aa54063d56218b3b868e93a7d
timf34 <farrelti@tcd.ie>
2023-09-11 14:33:15 +0100

    Only prints the first video from each day for each camera

--- temp_script.py
+++ temp_script.py
@@ -1,10 +1,5 @@
-# Temp script just to be able to list the video objects for each camera for each day
-
 import boto3
-import os
 from typing import List
-from tqdm import tqdm
-import botocore
 
 BUCKET_NAME: str = 'australia-fov'
 PATH_PREFIX: str = 'marvel/'
@@ -14,7 +9,7 @@ CAMERAS: List[str] = [
     "marvel-fov-2/",
     "marvel-fov-3/",
     "marvel-fov-4/",
-    "marvel-fov-5",
+    "marvel-fov-5/",
     "marvel-fov-6/",
     "marvel-fov-7/",
     "marvel-fov-8/",
@@ -26,17 +21,23 @@ PATH_POSTFIXES: List[str] = [
     "26_08_2023/"
 ]
 
-def main():
 
-    s3 = boto3.client('s3', region_name="ap-southeast-2")
+def main():
+    s3 = boto3.client('s3', region_name=REGION)
 
     for camera in CAMERAS:
         for postfix in PATH_POSTFIXES:
             # Constructing the path to the desired video
             camera_path = PATH_PREFIX + camera + postfix
-            # print(camera_path)
             response = s3.list_objects_v2(Bucket=BUCKET_NAME, Prefix=camera_path)
-            print(response)
+
+            # Filtering out only the video files and sorting them
+            video_files = sorted(
+                [content['Key'] for content in response.get('Contents', []) if content['Key'].endswith('.avi')])
+
+            # Print the first video from the list if available
+            if video_files:
+                print(video_files[0])
 
 
 if __name__ == "__main__":

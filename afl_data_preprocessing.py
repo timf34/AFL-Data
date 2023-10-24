aea6b5915622b7f1d4896d325fff7a6171d8119d
timf34 <farrelti@tcd.ie>
2023-10-24 15:19:43 +0100

    added clip_video_imageio function

--- afl_data_preprocessing.py
+++ afl_data_preprocessing.py
@@ -2,13 +2,14 @@
 This script will primarily split the .avi videos into individual frames
 """
 import cv2
+import imageio
 import os
 import subprocess
 from typing import List
 
 from utils import ensure_directory_exists, find_files_with_ending, png_files_exist, create_directory, mp4_files_exist
 
-AFL_DATA_DIR: str = r'marvel'
+AFL_DATA_DIR: str = r'marvel/marvel-fov-6'
 
 
 def extract_frames_from_video(file_path: str) -> None:
@@ -92,6 +93,47 @@ def clip_video(video, output_dir: str, clip_length: int) -> None:
         print(f"Finished clipping video {video} from {start_times[i]} to {end_times[i]}")
 
 
+def clip_video_imageio(video_path: str, output_dir: str, clip_length: int = 60) -> None:
+    """
+        Clip a video into multiple segments of the given length using imageio.
+
+        Parameters:
+        - video_path: Path to the video file.
+        - output_dir: Directory to store the clipped videos.
+        - clip_length: Duration of each clip in seconds (default is 60 seconds).
+        """
+    reader = imageio.get_reader(video_path)
+    fps = reader.get_meta_data()['fps']
+
+    # Calculate the number of frames needed for the specified clip length
+    frames_per_clip = int(clip_length * fps)
+
+    # Create the output directory if it doesn't exist
+    ensure_directory_exists(output_dir)
+
+    num_frames = len(reader)
+
+    for start_frame in range(0, num_frames, frames_per_clip):
+        end_frame = min(start_frame + frames_per_clip, num_frames)
+
+        # Define the output file name
+        output_file = os.path.join(output_dir, f"clip_{start_frame // frames_per_clip}.mp4")
+
+        # Check if this clip is too short (ignores the remainder)
+        if end_frame - start_frame < clip_length * 0.5 * fps:
+            break
+
+        print(f"Clipping video {video_path} from frame {start_frame} to {end_frame}")
+
+        # Write the segment to a new file
+        writer = imageio.get_writer(output_file, fps=fps, macro_block_size=1)
+        for frame in range(start_frame, end_frame):
+            writer.append_data(reader.get_data(frame))
+        writer.close()
+
+        print(f"Finished clipping video segment {output_file}")
+
+
 def extract_frames_from_all_videos(directory: str, file_ending: str = '.avi') -> None:
     """Given a directory, extract frames from all .avi videos in the directory."""
     avi_files = find_files_with_ending(directory, file_ending=file_ending)
@@ -124,8 +166,14 @@ def clip_all_videos_into_sixty_sec_clips(directory: str, file_ending: str = '.av
 
 
 def main():
-    extract_frames_from_all_videos(AFL_DATA_DIR, file_ending='.mp4')
+    # extract_frames_from_all_videos(AFL_DATA_DIR, file_ending='.mp4')
     # clip_all_videos_into_sixty_sec_clips(AFL_DATA_DIR)
+    clip_video_imageio(
+        video_path=r"C:\Users\timf3\PycharmProjects\AFL-Data\marvel\marvel-fov-6\18_08_2023\marvel_6_time_10_24_03_date_19_08_2023_.avi",
+        output_dir=r"C:\Users\timf3\PycharmProjects\AFL-Data\marvel\marvel-fov-6\18_08_2023\clipped",
+        clip_length=60
+    )
+
 
 if __name__ == '__main__':
     main()

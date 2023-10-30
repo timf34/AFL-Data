ba778e3440b6b853743bd5a7e75764bb746a0cf0
timf34 <farrelti@tcd.ie>
2023-10-30 16:51:40 +0000

    added functions for reading .xml files and getting the frame names with the balls

--- scripts/sagemaker_utils.py
+++ scripts/sagemaker_utils.py
@@ -133,3 +133,46 @@ def get_file_size_in_bytes(file_path: str) -> int:
     - Size of the file in bytes.
     """
     return os.path.getsize(file_path)
+
+
+def get_frames_containing_ball(xml_file_path: str) -> List[int]:
+    """
+    Parse an XML file containing ground truth annotations to identify frames which contain the ball.
+
+    This function specifically handles two XML structures:
+    1. XML structure with "frame" attributes typically found from videos uploaded to CVAT.
+    2. XML structure with "frame" attributes found from folders of images uploaded to CVAT.
+
+    Args:
+        xml_file_path (str): Path to the XML file containing ground truth annotations.
+
+    Returns:
+        List[int]: A list of unique frame numbers or image IDs containing the ball.
+    """
+    tree = ET.parse(xml_file_path)
+    root = tree.getroot()
+
+    frames_containing_ball = []
+
+    # Handle XML structure with "frame" attributes (from videos uploaded to CVAT)
+    for child in root:
+        for subchild in child:
+            if "frame" in subchild.attrib:
+                frame = int(subchild.attrib['frame'])
+                if frame not in frames_containing_ball:
+                    frames_containing_ball.append(frame)
+
+    # Handle XML structure with "frame" attributes (from folders of images uploaded to CVAT)
+    for image in root.findall('image'):
+        image_id = int(image.attrib['id'])
+
+        for points in image.findall('points'):
+            if points.attrib['label'] == 'ball' and image_id not in frames_containing_ball:
+                frames_containing_ball.append(image_id)
+
+    return frames_containing_ball
+
+
+def get_frame_name_from_frame_number(frame_number: int) -> str:
+    """Returns the name of the frame from the frame number"""
+    return f"frame_{frame_number:07d}.png"

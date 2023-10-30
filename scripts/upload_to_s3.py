7470c9d4a4579809b6b5f7d0cfb77e397b0bb064
timf34 <farrelti@tcd.ie>
2023-10-30 22:45:47 +0000

    script to upload a folder to S3

---
+++ scripts/upload_to_s3.py
@@ -0,0 +1,52 @@
+import boto3
+import os
+from botocore.exceptions import NoCredentialsError
+
+
+AUS_AFL_BUCKET: str = "australia-fov"
+DUB_AFL_BUCKET: str = "dublin-afl-preprocessed"
+ANNOTATIONS_FOLDER: str = "annotations"
+
+S3_PREFIX: str = "train/unpacked_png/"  # Ensure it ends with "/"
+
+# Ensure that each video path exists on s3
+s3 = boto3.client('s3')
+
+
+def upload_folder_to_s3(folder_path: str, bucket: str, prefix: str) -> None:
+    """
+    Uploads all .png files from a given folder to a specified S3 bucket and prefix.
+
+    Parameters:
+    - folder_path (str): Local path to the folder containing .png files
+    - bucket (str): Name of the S3 bucket to upload to
+    - prefix (str): S3 prefix (folder) to upload the files to
+
+    Returns:
+    - None
+    """
+    if not os.path.exists(folder_path):
+        print(f"Folder {folder_path} does not exist!")
+        return
+    else:
+        print(f"Uploading {folder_path} to {bucket}{prefix}")
+
+    for root, dirs, files in os.walk(folder_path):
+        for filename in files:
+            if filename.endswith('.png'):
+                local_path = os.path.join(root, filename)
+                s3_path = prefix + filename
+
+                try:
+                    s3.upload_file(local_path, bucket, s3_path)
+                    print(f"Uploaded {filename} to {bucket}/{s3_path}")
+                except NoCredentialsError:
+                    print("Credentials not available")
+
+
+def main():
+    upload_folder_to_s3("yolo_bitches", DUB_AFL_BUCKET, S3_PREFIX)
+
+
+if __name__ == '__main__':
+    main()

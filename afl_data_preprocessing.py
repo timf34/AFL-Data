12f88cd2634dc7d53254afd04f34b26628d6f364
timf34 <farrelti@tcd.ie>
2023-12-24 14:04:32 +0000

    refactored afl_data_preprocessing.py to work with direct video files and lists

--- afl_data_preprocessing.py
+++ afl_data_preprocessing.py
@@ -2,14 +2,20 @@
 This script will primarily split the .avi videos into individual frames
 """
 import cv2
-import imageio
+import math
 import os
 import subprocess
+
+from moviepy.editor import VideoFileClip
 from typing import List
 
 from utils import ensure_directory_exists, find_files_with_ending, png_files_exist, create_directory, mp4_files_exist
 
-AFL_DATA_DIR: str = r'marvel/marvel-fov-7'
+
+VIDEOS_TO_BE_CLIPPED: List[str] = [
+    "marvel\\marvel-fov-3\\26_08_2023\\marvel3_time_09_09_04_date_27_08_2023_clipped.mp4"
+]
+DIRECTORIES_FOR_FRAME_EXTRACTION: [str] = [r'marvel\marvel-fov-3\26_08_2023']
 
 
 def extract_frames_from_video(file_path: str) -> None:
@@ -40,7 +46,7 @@ def extract_frames_from_video(file_path: str) -> None:
     subprocess.run(cmd)
 
 
-def clip_video(video, output_dir: str, clip_length: int) -> None:
+def clip_video(video_path: str, output_dir: str, clip_duration: int = 60) -> None:
     """
     This function clips a video into multiple 60 second long clips.
     We use FFmpeg directly to ensure lossless clipping.
@@ -49,88 +55,64 @@ def clip_video(video, output_dir: str, clip_length: int) -> None:
     # Check if the output_directory exists, and if not, make it
     ensure_directory_exists(output_dir)
 
-    # Load the video and get its length
-    cv2_video = cv2.VideoCapture(video)
-    video_length: int = int(cv2_video.get(cv2.CAP_PROP_FRAME_COUNT))
-    fps: int = int(cv2_video.get(cv2.CAP_PROP_FPS))
+    # Load the video
+    video = VideoFileClip(video_path)
 
-    # Get the number of clips we need to make
-    num_clips: int = (video_length // fps) // clip_length
-
-    # Get the remainder
-    remainder: int = video_length % clip_length
-
-    # Get the start and end times for each clip
-    start_times: List[int] = [i * clip_length for i in range(num_clips)]
-
-    # If there is a remainder, add it to the end of the list
-    if remainder > 0:
-        start_times.append(num_clips * clip_length)
-
-    # Get the end times
-    end_times: List[int] = [i + clip_length for i in start_times]
+    # Calculate the number of clips
+    total_duration = video.duration
+    num_clips = math.ceil(total_duration / clip_duration)
 
     # Get the output names
-    output_names: List[str] = [output_dir + "\\" + str(i) + ".mp4" for i in range(len(start_times))]
+    output_names: List[str] = [output_dir + "\\" + str(i) + ".mp4" for i in range(num_clips)]
 
     # Clip the video
-    for i in range(len(start_times)):
-        print(f"Clipping video {video} from {start_times[i]} to {end_times[i]}")
+    for i in range(num_clips):
+        # Define the start and end of the clip
+        start = i * clip_duration
+        end = min((i + 1) * clip_duration, total_duration)
 
-        # Craft the FFmpeg command for lossless clipping
-        cmd = [
-            'ffmpeg',
-            '-i', video,
-            '-ss', str(start_times[i]),
-            '-t', str(clip_length),
-            '-c:v', 'copy',
-            '-an',  # This excludes the audio. If you want to include audio, you can remove this.
-            output_names[i]
-        ]
+        # Create the subclip
+        clip = video.subclip(start, end)
 
-        subprocess.run(cmd)
-
-        print(f"Finished clipping video {video} from {start_times[i]} to {end_times[i]}")
+        # Save the clip
+        clip_filename = f"{output_names[i]}"
+        clip.write_videofile(clip_filename, codec="libx264")
+        print(f"Clip {i + 1} saved: {clip_filename}")
 
 
-def extract_frames_from_all_videos(directory: str, file_ending: str = '.avi') -> None:
-    """Given a directory, extract frames from all .avi videos in the directory."""
-    avi_files = find_files_with_ending(directory, file_ending=file_ending)
-    for avi_file in avi_files:
-        extract_frames_from_video(avi_file)
+def extract_frames_from_all_videos(directories: List[str], file_ending: str = '.mp4') -> None:
+    """Given a directory, extract frames from all videos in the directory."""
+
+    for directory in directories:
+        avi_files = find_files_with_ending(directory, file_ending=file_ending)
+        for avi_file in avi_files:
+            extract_frames_from_video(avi_file)
 
 
-def clip_all_videos_into_sixty_sec_clips(directory: str, file_ending: str = '.avi') -> None:
+def clip_all_videos_into_sixty_sec_clips(video_paths: List[str]) -> None:
     """
     Given a directory, clip all .avi videos in the directory into 60 second clips.
     Store the new clipped videos in the same directory as the original full video in a folder of the same name.
     """
-    # Find all .avi files in the directory
-    avi_files = find_files_with_ending(directory, file_ending=file_ending)
-
-    for avi_file in avi_files:
+    for video_file in video_paths:
         # Create directory to store the clipped videos, name it the same as the original video
-        file_name_without_extension = os.path.splitext(os.path.basename(avi_file))[0]
-        file_dir = os.path.dirname(avi_file)
+        file_name_without_extension = os.path.splitext(os.path.basename(video_file))[0]
+        file_dir = os.path.dirname(video_file)
         clipped_videos_dir = create_directory(file_dir, directory_name=file_name_without_extension)
 
         # Check if clipped videos already exist in the directory
         if mp4_files_exist(clipped_videos_dir):
-            print(f'Clipped videos already exist in {clipped_videos_dir}. Skipping clipping for {avi_file}')
+            print(f'Clipped videos already exist in {clipped_videos_dir}. Skipping clipping for {video_file}')
             continue
 
         # Clip the video
-        clip_video(avi_file, clipped_videos_dir, 60)
+        clip_video(video_file, clipped_videos_dir, 60)
 
 
 def main():
-    # extract_frames_from_all_videos(AFL_DATA_DIR, file_ending='.mp4')
-    clip_all_videos_into_sixty_sec_clips(AFL_DATA_DIR)
-    # clip_video_imageio(
-    #     video_path=r"C:\Users\timf3\PycharmProjects\AFL-Data\marvel\marvel-fov-6\18_08_2023\marvel_6_time_10_24_03_date_19_08_2023_.avi",
-    #     output_dir=r"C:\Users\timf3\PycharmProjects\AFL-Data\marvel\marvel-fov-6\18_08_2023\clipped",
-    #     clip_length=60
-    # )
+    clip_all_videos_into_sixty_sec_clips(VIDEOS_TO_BE_CLIPPED)
+    extract_frames_from_all_videos(DIRECTORIES_FOR_FRAME_EXTRACTION, file_ending='.mp4')
+
     # extract_frames_from_video(
     #     file_path=r"C:\Users\timf3\PycharmProjects\AFL-Data\marvel\marvel-fov-6\18_08_2023\clipped\clip_0.mp4"
     # )

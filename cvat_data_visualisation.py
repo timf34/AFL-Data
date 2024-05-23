9d1898b586bb577c5494a73fab4f458a1ace105b
timf34 <farrelti@tcd.ie>
2024-05-23 14:46:18 +0100

    updated visualisation with frame offset and window resizing

--- cvat_data_visualisation.py
+++ cvat_data_visualisation.py
@@ -1,99 +1,87 @@
-"""
-This script takes a video and a cvat annotation file and visualises the annotations on the video.
-"""
 import cv2
-import os
 import xml.etree.ElementTree as ET
-
 from typing import Tuple, Dict
 
-VIDEO_PATH: str = r"C:\Users\timf3\PycharmProjects\AFL-Data\marvel\marvel-fov-8\18_08_2023\marvel8_time_10_24_04_date_19_08_2023_\1.mp4"
-CVAT_ANNOTATIONS: str = r"C:\Users\timf3\PycharmProjects\AFL-Data\marvel\afl-preprocessed\train\annotations\marvel8_time_10_24_04_date_19_08_2023_1.xml"
+# TODO: Any frame adjustments in here need to be mirrored in my training code!!!
+FRAME_OFFSET: int = 2
 
 
-def _load_cvat_groundtruth(xml_file_path: str) -> dict:
-    """
-    This function reads and loads the xml file into a dictionary which we will
-    then pass to _create_bohs_annotations...
+class VideoAnnotator:
+    def __init__(self, video_path: str, annotations_path: str, resize_factor: float = 1.0):
+        self.video_path = video_path
+        self.annotations_path = annotations_path
+        self.resize_factor = resize_factor
+        self.gt = self._load_cvat_groundtruth(annotations_path)
+        self.prev_frame = None
 
-    In general, this will read the groundtruth xml file, to extract frame and x-y position
+    def _load_cvat_groundtruth(self, xml_file_path: str) -> dict:
+        tree = ET.parse(xml_file_path)
+        root = tree.getroot()
+        gt = {"BallPos": []}
 
-    :params xml_file_path: path to the xml file
-    :returns: dictionary of ball positions
-    The structure of this dictionary is as follows:
-        {'BallPos': [[382, 382, 437, 782]
-        ... where the elements of the list are as follows [start_frame, end_frame, x, y]
-    """
+        for child in root:
+            for subchild in child:
+                if "frame" in subchild.attrib:
+                    frame = int(subchild.attrib['frame'])
+                    x, y = self._extract_coordinates(subchild)
+                    gt["BallPos"].append([frame, frame, x, y])
 
-    tree = ET.parse(xml_file_path)
-    root = tree.getroot()
+        for image in root.findall('image'):
+            image_id = int(image.attrib['id'])
+            for points in image.findall('points'):
+                if points.attrib['label'] == 'ball':
+                    x, y = self._extract_coordinates(points)
+                    gt["BallPos"].append([image_id, image_id, x, y])
 
-    gt = {"BallPos": []}
+        return gt
 
-    # Handle XML structure with "frame" attributes (from videos uploaded to CVAT)
-    for child in root:
-        for subchild in child:
-            if "frame" in subchild.attrib:
-                # print("frame:", subchild.attrib['frame'], "x:", subchild.attrib['points'].split(',')[0], "y:",  \
-                # subchild.attrib['points'].split(',')[1])
+    def _extract_coordinates(self, points) -> Tuple[int, int]:
+        x, y = map(float, points.attrib['points'].split(','))
+        print(self.resize_factor)
+        return int(x * self.resize_factor), int(y * self.resize_factor)
 
-                frame = int(subchild.attrib['frame'])
-                x, y = _extract_coordinates(subchild)
+    def annotate_video(self):
+        cap = cv2.VideoCapture(self.video_path)
+        paused = False
 
-                # We put frame in twice simply to match FootAndBall dataloader
-                gt["BallPos"].append([frame, frame, x, y])
+        while cap.isOpened():
+            if not paused:
+                ret, frame = cap.read()
+                if not ret:
+                    break
 
-    # Handle XML structure with "frame" attributes (from folders of images uploaded to CVAT)
-    for image in root.findall('image'):
-        image_id = int(image.attrib['id'])
+                frame = cv2.resize(frame, (0, 0), fx=self.resize_factor, fy=self.resize_factor)
 
-        for points in image.findall('points'):
-            if points.attrib['label'] == 'ball':
-                # Extracting x, y coordinates from points
-                x, y = _extract_coordinates(points)
-                # Append to the dictionary
-                gt["BallPos"].append([image_id, image_id, x, y])
-    return gt
+                if self.prev_frame is not None:
+                    for annotation in self.gt['BallPos']:
+                        start_frame, end_frame, x, y = annotation
+                        if start_frame <= cap.get(cv2.CAP_PROP_POS_FRAMES) - FRAME_OFFSET <= end_frame:
+                            cv2.circle(self.prev_frame, (x, y), int(10 * self.resize_factor), (0, 255, 0), -1)
 
+                    cv2.imshow('Frame', self.prev_frame)
 
-def _extract_coordinates(points) -> Tuple[int, int]:
-    x, y = map(float, points.attrib['points'].split(','))
-    return int(x), int(y)
+                self.prev_frame = frame
+
+            key = cv2.waitKey(30) & 0xFF
+            if key == ord('q'):
+                break
+            elif key == ord(' '):
+                paused = not paused
+
+        cap.release()
+        cv2.destroyAllWindows()
 
 
 def main():
-    # Load the video
-    cap = cv2.VideoCapture(VIDEO_PATH)
+    # video_path = r"C:\Users\timf3\PycharmProjects\AFL-Data\marvel\marvel-fov-6\19_05_2024\marvel-fov-6_time_04_21_07_date_19_05_2024_\2.mp4"
+    # annotations_path = r"C:\Users\timf3\Downloads\drive-download-20240523T024955Z-001\marvel-fov-6_time_04_21_07_date_19_05_2024_2\marvel-fov-6_time_04_21_07_date_19_05_2024_2.xml"
 
-    # Load the annotations
-    gt = _load_cvat_groundtruth(CVAT_ANNOTATIONS)
+    video_path = r"C:\Users\timf3\PycharmProjects\AFL-Data\marvel\marvel-fov-2\18_08_2023\marvel2_time_10_24_03_date_19_08_2023_\0.mp4"
+    annotations_path = r"C:\Users\timf3\PycharmProjects\AFL-Data\marvel\afl-preprocessed\train\annotations\marvel2_time_10_24_03_date_19_08_2023_0.xml"
 
-    # Iterate through the video
-    while cap.isOpened():
-        ret, frame = cap.read()
+    annotator = VideoAnnotator(video_path, annotations_path, resize_factor=0.8)
+    annotator.annotate_video()
 
-        if not ret:
-            break
-
-        # Draw the annotations on the frame
-        for annotation in gt['BallPos']:
-            start_frame, end_frame, x, y = annotation
-            if start_frame <= cap.get(cv2.CAP_PROP_POS_FRAMES) <= end_frame:
-                cv2.circle(frame, (x, y), 10, (0, 255, 0), -1)
-
-
-        # Display the frame
-        cv2.imshow('Frame', frame)
-
-        # Press 'q' to exit
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    # Release the VideoCapture and VideoWriter objects
-    cap.release()
-
-    # Destroy all windows
-    cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()

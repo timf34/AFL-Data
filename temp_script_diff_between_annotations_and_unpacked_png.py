ef8d83713655354d992949550a6f548aa6f3e759
timf34 <farrelti@tcd.ie>
2024-06-05 20:14:36 +0100

    removed having to hardcode anything

--- temp_script_diff_between_annotations_and_unpacked_png.py
+++ temp_script_diff_between_annotations_and_unpacked_png.py
@@ -1,30 +1,16 @@
 """
-Scripts for getting these, ran from python console in terminal in the same directory as annotaions and unpacked_png
-
-import os
-
-def list_directories():
-    cwd = os.getcwd()
-    return [d for d in os.listdir(cwd) if os.path.isdir(os.path.join(cwd, d))]
-
-print(list_directories())
-
-
-import os
-
-def list_xml_files():
-    cwd = os.getcwd()
-    return [f for f in os.listdir(cwd) if f.endswith('.xml')]
-
-print(list_xml_files())
+Lists the directories that are missing in the unpacked_png directory corresponding to the XML files in the annotations
+directory.
 """
+import os
 
-xml_files = ['marvel1_time_10_24_03_date_19_08_2023_0.xml', 'marvel1_time_10_24_03_date_19_08_2023_1.xml', 'marvel2_time_10_24_03_date_19_08_2023_0.xml', 'marvel2_time_10_24_03_date_19_08_2023_1.xml', 'marvel3_time_09_09_04_date_27_08_2023_0.xml', 'marvel3_time_10_24_03_date_19_08_2023_0.xml', 'marvel3_time_10_24_03_date_19_08_2023_1.xml', 'marvel7_time_10_24_03_date_19_08_2023_0.xml', 'marvel7_time_10_24_03_date_19_08_2023_1.xml', 'marvel8_time_10_24_04_date_19_08_2023_0.xml', 'marvel8_time_10_24_04_date_19_08_2023_1.xml', 'marvel_1_time_04_09_04_date_20_08_2023_0.xml', 'marvel_1_time_04_09_04_date_20_08_2023_3.xml', 'marvel_1_time_04_09_04_date_20_08_2023_4.xml', 'marvel_1_time_04_09_04_date_20_08_2023_5.xml', 'marvel_1_time_04_09_04_date_20_08_2023_6.xml', 'marvel_1_time_04_09_04_date_20_08_2023_7.xml', 'marvel_3_time_04_09_06_date_20_08_2023_0.xml', 'marvel_3_time_04_09_06_date_20_08_2023_1.xml', 'marvel_3_time_04_09_06_date_20_08_2023_2.xml', 'marvel_3_time_04_09_06_date_20_08_2023_3.xml', 'marvel_3_time_04_09_06_date_20_08_2023_5.xml', 'marvel_3_time_04_09_06_date_20_08_2023_7.xml', 'marvel_6_time_10_24_03_date_19_08_2023_0.xml', 'marvel_6_time_10_24_03_date_19_08_2023_1.xml', 'marvel_6_time_10_24_03_date_19_08_2023_2.xml', 'marvel_6_time_10_24_03_date_19_08_2023_3.xml', 'marvel_6_time_10_24_03_date_19_08_2023_4.xml', 'marvel_6_time_10_24_03_date_19_08_2023_5.xml', 'marvel_6_time_10_24_03_date_19_08_2023_6.xml', 'marvel_8_time_09_09_04_date_27_08_2023_0.xml', 'marvel_8_time_09_09_04_date_27_08_2023_2.xml', 'marvel_8_time_09_09_04_date_27_08_2023_3.xml', 'marvel_8_time_09_09_04_date_27_08_2023_4.xml', 'marvel_8_time_09_09_04_date_27_08_2023_5.xml', 'marvel_8_time_09_09_04_date_27_08_2023_7.xml']
+ANNOTATIONS_DIR: str = "marvel/afl-preprocessed/train/annotations"
+UNPACKED_PNG_DIR: str = "marvel/afl-preprocessed/train/unpacked_png"
 
-directories = ['marvel1_time_10_24_03_date_19_08_2023_0', 'marvel1_time_10_24_03_date_19_08_2023_1', 'marvel2_time_10_24_03_date_19_08_2023_0', 'marvel2_time_10_24_03_date_19_08_2023_1', 'marvel3_time_09_09_04_date_27_08_2023_0', 'marvel3_time_10_24_03_date_19_08_2023_0', 'marvel3_time_10_24_03_date_19_08_2023_1', 'marvel7_time_10_24_03_date_19_08_2023_0', 'marvel7_time_10_24_03_date_19_08_2023_1', 'marvel8_time_10_24_04_date_19_08_2023_0', 'marvel_1_time_04_09_04_date_20_08_2023_0', 'marvel_1_time_04_09_04_date_20_08_2023_3', 'marvel_1_time_04_09_04_date_20_08_2023_4', 'marvel_1_time_04_09_04_date_20_08_2023_5', 'marvel_1_time_04_09_04_date_20_08_2023_6', 'marvel_1_time_04_09_04_date_20_08_2023_7', 'marvel_3_time_04_09_06_date_20_08_2023_0', 'marvel_3_time_04_09_06_date_20_08_2023_1', 'marvel_3_time_04_09_06_date_20_08_2023_2', 'marvel_3_time_04_09_06_date_20_08_2023_3', 'marvel_3_time_04_09_06_date_20_08_2023_5', 'marvel_3_time_04_09_06_date_20_08_2023_7', 'marvel_6_time_10_24_03_date_19_08_2023_0', 'marvel_6_time_10_24_03_date_19_08_2023_1', 'marvel_6_time_10_24_03_date_19_08_2023_2', 'marvel_6_time_10_24_03_date_19_08_2023_3', 'marvel_6_time_10_24_03_date_19_08_2023_4', 'marvel_6_time_10_24_03_date_19_08_2023_5', 'marvel_6_time_10_24_03_date_19_08_2023_6', 'marvel_6_time_10_24_03_date_19_08_2023_7', 'marvel_8_time_09_09_04_date_27_08_2023_0', 'marvel_8_time_09_09_04_date_27_08_2023_2', 'marvel_8_time_09_09_04_date_27_08_2023_3', 'marvel_8_time_09_09_04_date_27_08_2023_4', 'marvel_8_time_09_09_04_date_27_08_2023_5', 'marvel_8_time_09_09_04_date_27_08_2023_7']
-
-# Strip the .xml extension from xml_files and create a set
+xml_files = [xml_file for xml_file in os.listdir(ANNOTATIONS_DIR) if xml_file.endswith('.xml')]
 xml_files_stripped = {file[:-4] for file in xml_files}
+
+directories = [dir for dir in os.listdir(UNPACKED_PNG_DIR) if os.path.isdir(os.path.join(UNPACKED_PNG_DIR, dir))]
 
 # Convert directories list to a set for comparison
 directories_set = set(directories)
